7b78a83c2b8889b78931df15ed6470324370dead
Dennis T <dennis@tychsen.me>
2015-11-23 20:46:35 +0100

    Implementation + test of 3 more methods

--- Vector/VectorCalculatorApp/VectorCalculatorApp/TestVectorCalculator.cs
+++ Vector/VectorCalculatorApp/VectorCalculatorApp/TestVectorCalculator.cs
@@ -53,5 +53,60 @@ namespace VectorCalculatorApp
             Assert.That(result.X, Is.EqualTo(2));
             Assert.That(result.Y, Is.EqualTo(-4));
         }
+
+        [Test]
+        public void TestScaleVector()
+        {
+            var vector = new Cartesian2DVector
+            {
+                X = 5,
+                Y = 3
+            };
+
+            var calc = new VectorCalculator();
+
+            int factor = 5;
+            var result = calc.Scale(vector, factor);
+
+            Assert.That(result.X, Is.EqualTo(25));
+            Assert.That(result.Y, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void TestVectorMagnitude()
+        {
+            var vector = new Cartesian2DVector
+            {
+                X = 3,
+                Y = 5
+            };
+
+            var calc = new VectorCalculator();
+
+            double result = calc.Magnitude(vector);
+
+            Assert.That(result, Is.EqualTo(Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y)));
+        }
+
+        [Test]
+        public void TestDotProduct()
+        {
+            var vector1 = new Cartesian2DVector
+            {
+                X = 5,
+                Y = 7
+            };
+
+            var vector2 = new Cartesian2DVector
+            {
+                X = 3,
+                Y = 2
+            };
+
+            var calc = new VectorCalculator();
+            int result = calc.DotProduct(vector1, vector2);
+
+            Assert.That(result, Is.EqualTo(29));
+        }
     }
 }
--- Vector/VectorCalculatorApp/VectorCalculatorApp/VectorCalculator.cs
+++ Vector/VectorCalculatorApp/VectorCalculatorApp/VectorCalculator.cs
@@ -17,13 +17,32 @@ namespace Vector
             return vec;
         }
 
-        internal Cartesian2DVector Subtract(Cartesian2DVector vector1, Cartesian2DVector vector2)
+        public Cartesian2DVector Subtract(Cartesian2DVector vector1, Cartesian2DVector vector2)
         {
             var vec = new Cartesian2DVector();
             vec.X = vector1.X - vector2.X;
             vec.Y = vector1.Y - vector2.Y;
 
             return vec;
+        }
+
+        public Cartesian2DVector Scale(Cartesian2DVector vector, int factor)
+        {
+            var vec = new Cartesian2DVector();
+            vec.X = vector.X * factor;
+            vec.Y = vector.Y * factor;
+
+            return vec;
+        }
+
+        public double Magnitude(Cartesian2DVector vector)
+        {
+            return Math.Sqrt(Math.Pow(vector.X, 2) + Math.Pow(vector.Y, 2));
+        }
+
+        public int DotProduct(Cartesian2DVector vector1, Cartesian2DVector vector2)
+        {
+            return (vector1.X * vector2.X) + (vector1.Y * vector2.Y);
         }
     }
 }
