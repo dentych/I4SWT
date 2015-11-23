7e9d147002d9dca2b433ef8fb3d916884d41efc0
Dennis T <dennis@tychsen.me>
2015-11-23 20:30:54 +0100

    Added VectorCalculator app + 2 methods/tests

---
+++ Vector/VectorCalculatorApp/VectorCalculatorApp/TestVectorCalculator.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vector;
+
+namespace VectorCalculatorApp
+{
+    [TestFixture]
+    class TestVectorCalculator
+    {
+        [Test]
+        public void TestAddTwoVectors()
+        {
+            Cartesian2DVector vector1 = new Cartesian2DVector
+            {
+                X = 5,
+                Y = 10
+            };
+            Cartesian2DVector vector2 = new Cartesian2DVector
+            {
+                X = 3,
+                Y = 14
+            };
+
+
+            VectorCalculator calc = new VectorCalculator();
+            Cartesian2DVector result = calc.Add(vector1, vector2);
+
+            Assert.That(result.X, Is.EqualTo(8));
+            Assert.That(result.Y, Is.EqualTo(24));
+        }
+
+        [Test]
+        public void TestSubtractTwoVectors()
+        {
+            Cartesian2DVector vector1 = new Cartesian2DVector
+            {
+                X = 5,
+                Y = 10
+            };
+            Cartesian2DVector vector2 = new Cartesian2DVector
+            {
+                X = 3,
+                Y = 14
+            };
+
+            var calc = new VectorCalculator();
+            Cartesian2DVector result = calc.Subtract(vector1, vector2);
+
+            Assert.That(result.X, Is.EqualTo(2));
+            Assert.That(result.Y, Is.EqualTo(-4));
+        }
+    }
+}
---
+++ Vector/VectorCalculatorApp/VectorCalculatorApp/VectorCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    public class VectorCalculator
+    {
+        public Cartesian2DVector Add(Cartesian2DVector vector1, Cartesian2DVector vector2)
+        {
+            var vec = new Cartesian2DVector();
+            vec.X = vector1.X + vector2.X;
+            vec.Y = vector1.Y + vector2.Y;
+
+            return vec;
+        }
+
+        internal Cartesian2DVector Subtract(Cartesian2DVector vector1, Cartesian2DVector vector2)
+        {
+            var vec = new Cartesian2DVector();
+            vec.X = vector1.X - vector2.X;
+            vec.Y = vector1.Y - vector2.Y;
+
+            return vec;
+        }
+    }
+}
